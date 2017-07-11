c3ef73e640d66047171dc972ebfabc7b9bc9ab1b
Yana Wilkins <yav9774@gmail.com>
2017-07-11 18:36:55 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,132 @@
+// The Form Data
+let formData = [
+  // For demonstration purposes,
+  // the first form element has been added to the HTML file as a comment
+  // compare the input in the HTML file with the contents of this first object
+  {
+    "type": "text",
+    "label": "First Name",
+    "id": "user-first-name",
+    "icon": "fa-user",
+    "options": []
+  },
+  {
+    "type": "text",
+    "label": "Last Name",
+    "id": "user-last-name",
+    "icon": "fa-user",
+    "options": []
+  },
+  {
+    "type": "email",
+    "label": "Email Address",
+    "id": "user-email",
+    "icon": "fa-envelope",
+    "options": []
+  },
+  {
+    "type": "text",
+    "label": "Current Website URL",
+    "id": "user-website",
+    "icon": "fa-globe",
+    "options": []
+  },
+  {
+    "type": "select",
+    "label": "Select Language",
+    "id": "user-language",
+    "icon": "",
+    "options": [
+      {
+        "label": "English",
+        "value": "EN"
+      },
+      {
+        "label": "French",
+        "value": "FR"
+      },
+      {
+        "label": "Spanish",
+        "value": "SP"
+      },
+      {
+        "label": "Chinese",
+        "value": "CH"
+      },
+      {
+        "label": "Japanese",
+        "value": "JP"
+      }
+    ]
+  },
+  {
+    "type": "textarea",
+    "label": "Your Comment",
+    "id": "user-comment",
+    "icon": "fa-comments",
+    "options": []
+  },
+  {
+    "type": "tel",
+    "label": "Mobile Number",
+    "id": "user-mobile",
+    "icon": "fa-mobile-phone",
+    "options": []
+  },
+  {
+    "type": "tel",
+    "label": "Home Number",
+    "id": "user-phone",
+    "icon": "fa-phone",
+    "options": []
+  }
+];
+
+// get the parent
+let fields = document.querySelector('.fields')
+// create element
+// set it up, setting any attributes
+
+for (var i = 0; i < formData.length; i++) {
+  formData[i]
+  if (formData[i].type === 'select') {
+    // make a select tag
+    let select = document.createElement('select');
+    for (let j = 0; j < formData[i].options.length; j++){
+      let language = document.createElement('option')
+      language.value = formData[i].options[j].value
+      language.innerHTML = formData[i].options[j].label
+      select.appendChild(language)
+    }
+    // innerHTML - sets value of whatever is in the element
+    fields.appendChild(select)
+  }
+
+
+  else {
+    let form1 = document.createElement( 'input' );
+    form1.type = formData[i].type
+    form1.placeholder = formData[i].label
+    form1.id = formData[i].id
+    fields.appendChild(form1)
+  }
+}
+// append it to the parent
+
+
+
+
+// HINTS:
+// As you can see, we access the first element in the array
+// with [0] and then grab the property "label" using the "." operator
+// ( function(){
+//   // Select the first element from the array
+//   let first = formData[ 0 ];
+//   // Log the first object
+//   console.log( first );
+//   // Log the string "First Name"
+//   console.log( first.label );
+// } )();
+
+
+// -------- Your Code Goes Below this Line --------
